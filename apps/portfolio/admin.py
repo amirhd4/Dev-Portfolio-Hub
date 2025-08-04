e84d79fb0fae5d3065adf4d6d3481eee50770d11
amirhd4 <amir0hd4@gmail.com>
2025-08-04 16:58:26 +0330

    Configure and customize Django admin panel

--- apps/portfolio/admin.py
+++ apps/portfolio/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Technology, Service, Project, Testimonial
 
-# Register your models here.
+# برای نمایش گواهی‌نامه‌ها در صفحه پروژه
+class TestimonialInline(admin.TabularInline):
+    model = Testimonial
+    extra = 1 # تعداد فرم خالی برای اضافه کردن آیتم جدید
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'live_demo_url') # فیلدهایی که در لیست نمایش داده می‌شوند
+    list_filter = ('technologies', 'services') # فیلدهایی برای فیلتر کردن در سایدبار
+    search_fields = ('title', 'description') # فیلدهایی که قابل جستجو هستند
+    inlines = [TestimonialInline] # اضافه کردن ویرایش گواهی‌نامه‌ها در همین صفحه
+
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+# مدل‌های دیگر را به صورت ساده ثبت می‌کنیم
+admin.site.register(Service)
+admin.site.register(Testimonial)
