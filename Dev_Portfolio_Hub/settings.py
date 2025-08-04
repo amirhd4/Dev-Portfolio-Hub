f248ec24562edbb8368efc297b79984ae8b7118a
amirhd4 <amir0hd4@gmail.com>
2025-08-04 21:06:50 +0330

    Integrate Tailwind CSS with Django templates

--- Dev_Portfolio_Hub/settings.py
+++ Dev_Portfolio_Hub/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'Dev_Portfolio_Hub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,6 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
